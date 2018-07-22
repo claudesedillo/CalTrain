061a213728bc0b4342b784b26b7dfe28fde2e224
claudesedillo <claudesedillo@gmail.com>
2018-07-23 04:01:21 +0800

    Initial commit

---
+++ src/locks/Driver.java
@@ -0,0 +1,19 @@
+package locks;
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author paolo
+ */
+public class Driver {
+	
+
+	public static void main(String[] args){
+		GUI gui = new GUI();
+	}
+
+    
+}
+
---
+++ src/locks/StaticTexts.java
@@ -0,0 +1,10 @@
+package locks;
+
+public class StaticTexts {
+
+	public static String train_default_status = "Status: Not Deployed";
+	public static String train_deployed_status = "Status: Deployed ";
+	public static String train_passenger_count = "Passenger Count: ";
+	public static String train_seat_limit = "Seat Limit: ";
+	public static String train_current_location = "Location: ";
+}
